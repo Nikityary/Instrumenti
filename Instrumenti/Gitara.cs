bce8f18305fa304e62d44c8e855d81141d0e6fc7
Никита <nikityary@mail.ru>
2022-09-12 19:26:37 +0300

    Istruments

---
+++ Instrumenti/Gitara.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instrumenti
+{
+    internal class Gitara : Instrument
+    {
+        private int strNumber = 6;
+
+        public void setStrNumber(int strNumber) 
+        { 
+            this.strNumber = strNumber; 
+        }
+
+        public int getStrNumber() 
+        { 
+            return strNumber; 
+        }
+
+        public void play() 
+        { 
+            Console.WriteLine("Гитара играет. " + toString()); 
+        }
+
+        public String toString() 
+        { 
+            return "Количество струн: " + strNumber; 
+        }
+    }
+}
